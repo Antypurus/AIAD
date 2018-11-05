9c4f2a6e42280165ed27b53ab706b5e1594e1ef3
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-11-05 11:23:24 +0000

    started working on company agent

--- src/Company/Agents/CompanyAgent.java
+++ src/Company/Agents/CompanyAgent.java
@@ -1,4 +1,35 @@
 package Company.Agents;
 
-public class CompanyAgent {
+import Aggregators.Index;
+import Common.Date;
+import Company.Company;
+import Components.Yield;
+import jade.core.Agent;
+
+public class CompanyAgent extends Agent {
+
+    private Company company;
+
+    public void setup() {
+        Object[] args = this.getArguments();
+
+        String name = args[0].toString();
+        String acronym = args[1].toString();
+        Index index = (Index) args[2];
+        double qualityBias = (Double) args[3];
+        Date foundationDate = (Date) args[4];
+        Yield yield = null;
+        if (args.length >= 6) {
+            yield = (Yield) args[5];
+        }
+
+        if (yield == null) {
+            this.company = new Company(name, acronym, index, qualityBias,
+                    foundationDate);
+        } else {
+            this.company = new Company(name, acronym, index, qualityBias,
+                    yield, foundationDate);
+        }
+    }
+
 }
