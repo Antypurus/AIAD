1a22adf58ca2424722dca6f4435bcd7b10fb3f24
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-11-08 12:14:03 +0000

    added classes for behaviors of buys stock

---
+++ src/Investor/Behavior/BuyStockFromAgentBehavior.java
@@ -0,0 +1,18 @@
+package Investor.Behavior;
+
+import jade.core.behaviours.Behaviour;
+
+public class BuyStockFromAgentBehavior extends Behaviour
+{
+    @Override
+    public void action()
+    {
+
+    }
+
+    @Override
+    public boolean done()
+    {
+        return false;
+    }
+}
---
+++ src/Investor/Behavior/BuyStockFromCompanyBehavior.java
@@ -0,0 +1,18 @@
+package Investor.Behavior;
+
+import jade.core.behaviours.Behaviour;
+
+public class BuyStockFromCompanyBehavior extends Behaviour
+{
+    @Override
+    public void action()
+    {
+
+    }
+
+    @Override
+    public boolean done()
+    {
+        return false;
+    }
+}
